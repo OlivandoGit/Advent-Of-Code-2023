d647367bbcf5cb069450071b319c562f994b7059
Olivando <72449057+OlivandoGit@users.noreply.github.com>
2023-12-10 18:07:05 +0000

    Completed day 9

---
+++ Day 09/Part 2.py
@@ -0,0 +1,23 @@
+histories = []
+with open("Input.txt", "r") as file:
+    histories = [[int(x) for x in line.strip().split()[::-1]] for line in file]
+
+for history in histories:
+    sequences = [history]
+
+    while sum(x != 0 for x in sequences[-1]) != 0:
+        sequence = []
+        for i, num in enumerate(sequences[-1][:-1]):
+            sequence.append(sequences[-1][i + 1] - num)
+
+        sequences.append(sequence)
+
+    for i, sequence in enumerate(sequences[::-1][:-1]):
+        sequences[len(sequences) - i - 2].append(sequences[len(sequences) - i - 2][-1] + sequence[-1])
+
+    print(sequences)
+
+for history in histories:
+    print(history)
+
+print(sum([history[-1] for history in histories]))
